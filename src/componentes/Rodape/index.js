451ee70396c7a9af9908420ae517edd809cfcd9f
Ctest704 <cgithub9@gmail.com>
2025-02-26 15:28:16 -0300

    atualização para mudanças de cores

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -27,7 +27,7 @@ const Rodape = () => {
        </section>
        <section>
         <p>
-            Desenvolvido por Alura.
+            Desenvolvido por Fernando.
         </p>
        </section>
     </footer>)
